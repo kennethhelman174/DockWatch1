2153bf6e43a9ddfa09b068d8a1ffe5d42c8c21e0
kennethhelman174 <88667676+kennethhelman174@users.noreply.github.com>
2025-06-19 20:41:29 -0500

    Delete src/ai directory

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/estimate-arrival-time.ts';
-import '@/ai/flows/get-weather-forecast-flow.ts';
-import '@/ai/flows/app-support-flow.ts';
-import '@/ai/flows/get-daily-safety-tip-flow.ts'; // Added new flow
